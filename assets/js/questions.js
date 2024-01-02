b6ec5ef1da0fd7f67d7ced8d52038aa4011ed02c
IgorTraspadini <igorchieppe@hotmail.com>
2024-01-02 08:32:19 -0300

    questions script changed done!

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+const questions = {
+  1: {
+    "question-title": "What is the correct file extension for Python files?",
+    "choices": [".pyt",".pyth",".pt",".py"],
+    "answer": 4
+  },
+  2: {
+    "question-title": "What is the correct syntax to output the type of a variable or object in Python?",
+    "choices": ["print(typeof x)","print(typeof(x))","print(type(x))","print(typeOf(x))"],
+    "answer": 3
+  },
+  3: {
+    "question-title": "Which method can be used to remove any whitespace from both the beginning and the end of a string?",
+    "choices": ["len()","trim()","strip()","ptrim()"],
+    "answer": 2
+  },
+  4: {
+    "question-title": "Which method can be used to return a string in upper case letters?",
+    "choices": ["toUpperCase()","upperCase()","uppercase()","upper()"],
+    "answer": 4
+  },
+  5: {
+    "question-title": "Which method can be used to replace parts of a string?",
+    "choices": ["repl()","switch()","replace()","replaceString()"],
+    "answer": 3
+  },
+}
+
